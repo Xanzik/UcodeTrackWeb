89caaefea43e52f773e967862a168993154c5db3
Xanzik <46788575+Xanzik@users.noreply.github.com>
2023-11-15 20:24:09 +0300

    DB auto-create update

--- Connect-FullStack usof-backend/utils/db_s.js
+++ Connect-FullStack usof-backend/utils/db_s.js
@@ -1,6 +1,26 @@
 import { Sequelize } from 'sequelize';
 import config from './config.json' assert { type: "json" };
 
+const sequelize_db = new Sequelize('', config.user, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+});
+
+async function initDatabase() {
+  try {
+    await sequelize_db.authenticate();
+    console.log('Connection to MySQL server has been established successfully.');
+    await sequelize_db.query(`CREATE DATABASE IF NOT EXISTS ${config.database}`);
+    console.log('Database has been created or already exists.');
+    await sequelize_db.query(`USE ${config.database}`);
+    console.log('Using the database:', config.database);
+  } catch (error) {
+    console.error('Unable to connect to MySQL server:', error);
+  }
+}
+
+await initDatabase();
+
 const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
   dialect: 'mysql',
